e8301ed4f819f7dbd711fb7723bc652d33201b55
Nitish pandey <153537473+Nitish7040@users.noreply.github.com>
2024-09-16 23:02:56 +0530

    add todo with redux

--- 10_redux_toolkit/src/App/Store.js
+++ 10_redux_toolkit/src/App/Store.js
@@ -1,3 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit'
+import todReducer from '../Features/Todo/todoslice'
 
-export const store = configureStore({});
+export const store = configureStore({
+    reducer : todReducer
+})
+ 
