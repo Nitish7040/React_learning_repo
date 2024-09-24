05feb92f05e58276f8f42f3c55b028b044e4ba11
Nitish pandey <153537473+Nitish7040@users.noreply.github.com>
2024-09-24 21:12:25 +0530

    starting of mega proj

---
+++ 11_mega_proj/src/Conf/Conf.js
@@ -0,0 +1,11 @@
+const Conf ={
+    appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE_URL),
+    appwriteProjectid: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteCollectionid: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteDatabaseid: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteBucketid: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+
+
+export default Conf;
