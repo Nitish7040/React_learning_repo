3ad89001380772488901ee8b046d14fb5d3236f8
Nitish pandey <153537473+Nitish7040@users.noreply.github.com>
2024-10-13 02:09:23 +0530

    add some files

--- 11_mega_proj/src/App.jsx
+++ 11_mega_proj/src/App.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import AuthSevice from './Appwrite/auth'
+import {login ,logout} from "./Store/authSlice"
+
 import './App.css'
 
 function App() {
- console.log(import.meta.env.VITE_APP_APPWRITE_URL);
- 
-  return (
-    <>
-      <h1>Blog with app write</h1>
-    </>
-  )
+  const [loading ,setloading] = useState(true)
+  const dispatch = useDispatch()
+
+  // use useeffect to ask login is loggedin or not..
+  useEffect(() =>{
+    AuthSevice.getcurrentuser()
+    .then( (userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .finally( () => setloading(false))
+  },[])
+
+  
+  /// conditional rendering use
+  // return !loading ? ( ) : (null)
+
+return !loading ? (
+  <div className='min-h-screen'>
+test 
+  </div>
+):null
 }
 
 export default App
